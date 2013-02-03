25bf80de12797cf427e2546e83f7a4d76740e6e7
Bastian Venthur <venthur@debian.org>
2013-02-03 12:17:07 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+
+setup(name='gscholar',
+      version='0.1',
+      description='Python library to query Google Scholar.',
+      author='Bastian Venthur',
+      author_email='mail@venthur.de',
+      url='http://github.com/venthur/gscholar',
+      packages=['gscholar'],
+)
