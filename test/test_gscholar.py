8e00b733879a54f6494af729a43ab5c3035760a5
Bastian Venthur <venthur@debian.org>
2014-10-14 09:18:14 +0200

    Added preliminary unittests for latin vs utf queries.

---
+++ test/test_gscholar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import unittest
+
+from gscholar import gscholar as gs
+
+
+class TesteGScholar(unittest.TestCase):
+
+    def test_query(self):
+        """Normal query with latin encoding should give non empty result."""
+        result = gs.query('Albert Einstein', gs.FORMAT_BIBTEX)
+        self.assertTrue(result)
+
+    def test_query_utf8(self):
+        """Normal query with utf8 encoding should give non empty result."""
+        result = gs.query("Anders Jonas Ångström", gs.FORMAT_BIBTEX)
+        self.assertTrue(result)
