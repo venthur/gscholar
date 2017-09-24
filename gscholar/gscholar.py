bc02ec3e0155b6689f2ddb470b32f4ace4f08b0a
Bastian Venthur <venthur@debian.org>
2017-09-24 19:39:11 +0200

    Updated base URL to use https

--- gscholar/gscholar.py
+++ gscholar/gscholar.py
@@ -55,7 +55,7 @@ import logging
 rand_str = str(random.random()).encode('utf8')
 google_id = hashlib.md5(rand_str).hexdigest()[:16]
 
-GOOGLE_SCHOLAR_URL = "http://scholar.google.com"
+GOOGLE_SCHOLAR_URL = "https://scholar.google.com"
 # the cookie looks normally like:
 #        'Cookie' : 'GSP=ID=%s:CF=4' % google_id }
 # where CF is the format (e.g. bibtex). since we don't know the format yet, we
