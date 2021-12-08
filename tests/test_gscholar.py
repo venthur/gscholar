5c391b8265bf626209a9d1a04205b53ad07aaa59
Bastian Venthur <bastian.venthur@flixbus.com>
2021-12-08 13:38:13 +0100

    allow the rate limited tests to fail

--- tests/test_gscholar.py
+++ tests/test_gscholar.py
@@ -1,12 +1,16 @@
 from gscholar import gscholar as gs
 
+import pytest
 
+
+@pytest.mark.xfail(reason="Google's rate limiter.")
 def test_query():
     """Normal query with latin encoding should give non empty result."""
     result = gs.query('Albert Einstein', gs.FORMAT_BIBTEX)
     assert len(result) > 0
 
 
+@pytest.mark.xfail(reason="Google's rate limiter.")
 def test_query_utf8():
     """Normal query with utf8 encoding should give non empty result."""
     result = gs.query("Anders Jonas Ångström", gs.FORMAT_BIBTEX)
